97e584e39480d8a6fa53a615edcfe5f024bd4738
Koss Wong <kosswong@gmail.com>
2021-02-27 00:04:15 +0800

    Completed frontend for phase 2

--- product.php
+++ product.php
@@ -1,11 +1,28 @@
-<?php require 'header.php'; ?>
+<?php
+require 'header.php';
+
+$link = mysqli_connect("localhost", "root", "", "test");
+
+$pid = isset($_GET['pid']) ? $_GET['pid'] : 1;
+$sql = "SELECT products.*, categories.name as cname FROM products JOIN categories ON products.catid = categories.catid WHERE pid='".$pid."' LIMIT 1 ";// LIMIT 3
+if ($result = $link -> query($sql)) {
+    while ($row = $result->fetch_row()) {
+        $pid = $row[0];
+        $catid = $row[1];
+        $name = $row[2];
+        $price = $row[3];
+        $description = $row[4];
+        $catname = $row[5];
+    }
+}
+?>
 
 <main class="container">
     <nav aria-label="breadcrumb" id="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-            <li class="breadcrumb-item"><a href="index.html">Category 1</a></li>
-            <li aria-current="page" class="breadcrumb-item active">ORGANIC SENCHA TEA BAG</li>
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item"><a href="index.php?catid=<?php echo $catid?>"><?php echo $catname?></a></li>
+            <li aria-current="page" class="breadcrumb-item active"><?php echo $name?></li>
         </ol>
     </nav>
 </main>
@@ -15,15 +32,14 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-6">
-                <img alt="..." class="card-img-top" src="img/product/8994473017374.jpg">
+                <img alt="..." class="card-img-top" src="img/product/o/<?php echo $pid?>.jpg">
             </div>
             <div class="col-12 col-md-6">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">ORGANIC SENCHA TEA BAG</h5>
-                        <p class="card-text"><s>HK$45.90</s> $38.00</p>
-                        <p>Mauris viverra cursus ante laoreet eleifend. Donec vel fringilla ante. Aenean finibus velit
-                            id urna vehicula, nec maximus est sollicitudin.</p>
+                        <h5 class="card-title"><?php echo $name?></h5>
+                        <p class="card-text">$<?php echo $price?></p>
+                        <p><?php echo $description?></p>
                         <form class="form-inline">
                             <div class="form-group mx-sm-3 mb-2">
                                 <label for="amount">Amount</label>
